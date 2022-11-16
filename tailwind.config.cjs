c15e91666b26d754fc1c97cf3be5c8d175ccd6a9
artifactz1 <punsalang.art@gmail.com>
2022-11-16 00:34:03 -0800

    fixed / setup t3 stack

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        yeezy_regular: ["YEEZY-REGULAR", "cursive"],
+        yeezy_light: ["YEEZY-LIGHT", "cursive"],
+        yeezy_heavy: ["YEEZY-HEAVY", "cursive"],
+        yeezy_bold: ["YEEZY-BOLD", "cursive"],
+        yeezy_mono: ["YEEZY-MONO", "cursive"],
+        liberation_mono: ["LIB-REG", "cursive"],
+        liberation_bold: ["LIB-BOLD", "cursive"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // require("tailwind-scrollbar-hide"),
+    require("daisyui"),
+
+    // add this to your plugins
+  ],
 };
